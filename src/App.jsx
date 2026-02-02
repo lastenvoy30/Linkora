583e52bbea962e2bfeff8a5810041f3c09c297f0
Mirza Isfand <mirzaisfand321@gmail.com>
2026-02-02 20:17:09 +0530

    added header footer and sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import './App.css'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/header";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
 
 function App() {
-
   return (
     <>
-    <div>hi</div>
+      <div className="app-container">
+        <Sidebar></Sidebar>
+        <div className="content">
+          <Header></Header>
+          <Footer></Footer>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
