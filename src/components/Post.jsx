b7853101e9346ee2e0bf0001d9b23091c555d798
Mirza Isfand <mirzaisfand321@gmail.com>
2026-02-09 14:41:42 +0530

    majority of ui is done

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,17 +1,27 @@
-const Post = () => {
+import { MdDelete } from "react-icons/md";
 
-    return (
-        <>
-        <div className="card" style={{width: "18rem"}}>
-  <img src="..." className="card-img-top" alt="..." />
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-        </>
-    )
-}
+const Post = ({ post }) => {
+  return (
+    <>
+      <div className="card post-card" style={{ width: "30rem" }}>
+        <div className="card-body">
+          <h5 className="card-title">
+            {post.title}
+            <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+              <MdDelete />
+            </span>
+          </h5>
+          <p className="card-text">{post.body}</p>
+          {post.tags.map((tag) => (
+            <span className="badge text-bg-primary hashtag">{tag}</span>
+          ))}
+          <div class="alert alert-success reactions" role="alert">
+            This post was reacted by {post.reactions} people
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Post;
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -4,12 +4,11 @@ import { PostList as PostListData } from "../store/post-list-store";
 
 const PostList = () =>{
     const { postList } = useContext(PostListData); 
-    console.log(postList);
 
     return (
         <>
         {postList.map((post) => (
-            <Post />
+            <Post key={post.id} post = {post} />
         ))}
         </>
     )
