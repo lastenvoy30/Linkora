ce3d2daf9fa69f90dce7fd7fc40c618d4eaee122
Mirza Isfand <mirzaisfand321@gmail.com>
2026-02-14 22:17:05 +0530

    added loading state

--- src/components/WelcomeMessage.jsx
+++ src/components/WelcomeMessage.jsx
@@ -1,11 +1,8 @@
-const WelcomeMessage = ({onGetPostsClick}) => {
+const WelcomeMessage = ({}) => {
   return (
     <center className="welcome-message">
       {" "}
       <h1>No posts to display</h1>
-      <button type="button" onClick={onGetPostsClick} className="btn btn-success">
-        Get Posts from Server
-      </button>
     </center>
   );
 };
